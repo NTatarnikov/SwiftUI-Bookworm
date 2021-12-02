45fc1e2783dd78bd07bf36ec2e42a30d06c28b02
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-12-03 00:08:16 +0300

    Initial Commit

---
+++ SwiftUI-Bookworm/SwiftUI_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BookwormApp.swift
+//  SwiftUI-Bookworm
+//
+//  Created by Nikita Tatarnikov on 03.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
